9f2018ef4c3461279039bc0f21375f3f0f5de18c
Felipe Barbosa de Oliveira <felipe.boliveira@outlook.com>
2019-12-12 14:47:14 -0300

    Cria modulos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
