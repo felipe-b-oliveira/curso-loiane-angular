bdaced0d9e498ae4f17fa56e3b4dfeca301890ef
felipe-b-oliveira <felipe.boliveira@outlook.com>
2019-12-20 15:01:42 -0300

    Finaliza Emitindo Eventos com Output Properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -27,6 +27,8 @@ export class DataBindingComponent implements OnInit {
 
   nomeDoCurso: string = 'Angular';
 
+  valorInicial = 15;
+
   getValor() {
     return 1;
   }
@@ -54,6 +56,10 @@ export class DataBindingComponent implements OnInit {
 
   onMouseOverOut() {
     this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento) {
+    console.log(evento.novoValor);
   }
 
 }
