7ca80109681788ae68eba620493f3426dd61d81a
felipe-b-oliveira <felipe.boliveira@outlook.com>
2019-12-23 14:49:41 -0300

    Configura Pre-Processadores

---
+++ 04-diretivas/src/app/diretiva-ngif/diretiva-ngif.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+// Decorator para a injeção de dependência do serviço
+@Injectable({
+  providedIn: 'root'
+})
+export class DiretivaNgifService {
+
+  constructor() { }
+}
